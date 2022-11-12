28b7f965f4bc9cf9dea2b8ab3fbd98f1a311079e
Ashu Singh <ashu.ask.singh@gmail.com>
2022-11-12 13:22:55 +0530

    Starting 'fault tolerance' and 'resilience' from here

--- rating-data-service/src/main/java/com/singashi/ratingdataservice/RatingDataServiceApplication.java
+++ rating-data-service/src/main/java/com/singashi/ratingdataservice/RatingDataServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class RatingDataServiceApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RatingDataServiceApplication.class, args);
 	}
